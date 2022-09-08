101dbca2588e18e0458edf4b536ab3271109d358
Cesar <30559667+CesarAroon@users.noreply.github.com>
2022-09-08 16:31:51 -0500

    Add files via upload

---
+++ Paz_y_amor_Spring/src/main/java/mx/com/CESAR/AmorYPazApplication.java
@@ -0,0 +1,14 @@
+package mx.com.CESAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class AmorYPazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmorYPazApplication.class, args);
+	}
+
+}
---
+++ Paz_y_amor_Spring/src/test/java/mx/com/CESAR/AmorYPazApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.CESAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmorYPazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
